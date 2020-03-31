0b9d8e6104d84d58b36525aaecec6c16fc5ca7c1
kebard1988@gmail.com <Qq8972060>
2020-03-31 13:31:38 +0300

    Отредактировал

--- yrok/Program.cs
+++ yrok/Program.cs
@@ -6,9 +6,7 @@ namespace yrok
     {
         static void Main(string[] args)
         {
-            int x = 5 * 6;
-
-            Console.WriteLine(x);
+            Console.WriteLine(Free(33));
             Console.ReadKey();
         }
 
