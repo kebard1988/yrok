27d1f9b38b95d38327217fac14e31737e9ac4021
kebard1988@gmail.com <Qq8972060>
2020-03-31 13:10:13 +0300

    Добавьте файлы проекта.

---
+++ yrok/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace yrok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5 * 6;
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
